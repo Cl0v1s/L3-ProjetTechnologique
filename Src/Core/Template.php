ec64b76bda6c2ea3bbd42bdbb5f0831aadb5c078
chaipokoi <chaipokoi@gmail.com>
2017-01-21 13:59:29 +0100

    Finalisation du système de templaye
    Signed-off-by: chaipokoi <chaipokoi@gmail.com>

--- Src/Core/Template.php
+++ Src/Core/Template.php
@@ -8,49 +8,81 @@
  */
 class Template
 {
+
     /**
-     * Lit un fichier tempmlate et produit du code html en fonction de $data
-     * @param $file
-     * @param $data
+     * Lit un fichier de template $file et retourne son contenu
+     * @param string $file fichier de template à lire
+     * @return string Contenu du fichier
      */
-    public static function prepare($file, $data)
+    public static function open($file)
     {
         $content = file_get_contents("Templates/".$file);
+        return $content;
+    }
+
+    /**
+     * Lit le contenu d'un template et produit du code html en fonction de $data
+     * @param string $content contenu du template de base
+     * @param array $data Dictionnaire de données à insérer dans le template
+     * @param bool $fatal si vrai, arrete lors de la rencontre d'une erreur fatale
+     * @return mixed contenu du template modifié
+     * @throws Exception
+     */
+    public static function prepare($content, $data, $fatal = true)
+    {
+
         $matches = array();
 
         // recherche et concaténation des includes
-        preg_match_all("<#(.*?)#>", $content, $matches);
+        preg_match_all("/<@(.*?)>/", $content, $matches);
         foreach($matches[1] as $match)
         {
-            $key = str_replace(" ", "", $match);
-            $content = preg_replace("(<#".$match."#>)", Template::prepare($key, $data), $content, 1);
+            $key = str_replace(" ", "", $match).".html";
+            $inner_content = Template::open($key);
+            $inner_content = Template::prepare($inner_content, $data);
+            $content = preg_replace("/(<@".$match.">)/", $inner_content, $content, 1);
         }
 
         // recherche et expansion des boucles
-        preg_match_all("<%(.*?)%>(.*?)</%>", $content, $matches);
-        for($i = 0; $i != count($matches[1]); $i++)
+
+        $reg = "/<#(.*?)>\s*?(.*?)\s*?<\/#(.*?)>/";
+        preg_match_all($reg, $content, $matches);
+        for($i = 0; $i!= count($matches[1]); $i++)
         {
-            $ma = array();
-            $match = $matches[1][$i];
-            $inside = $matches[2][$i];
-            preg_match_all("<=(.*?)=>", $inside, $ma);
-            foreach($ma[1] as $mat)
+            $array_name = str_replace(" ", "", $matches[1][$i]);
+
+            if(isset($data[$array_name]) == false)
             {
-                $key = str_replace(" ", "", $mat);
-                $content = preg_replace("(<=".$mat."=>)", $data[$key], $content, 1);
+                throw new Exception($array_name." not specified in data.");
             }
+            if(is_array($data[$array_name]) == false)
+                continue;
+            $body = "";
+
+            for($u = 0; $u != count($data[$array_name]); $u++)
+            {
+                $body = $body.$matches[2][$i];
+                $body = Template::prepare($body, $data[$array_name][$u], false);
+            }
+            $content = preg_replace("/<#(.*?)>\s*?(.*?)\s*?<\/#(.*?)>/", $body, $content, 1);
+
         }
 
         // recherche et remplacement des données
         //recherche des variables
-        preg_match_all("<=(.*?)=>", $content, $matches);
+        preg_match_all("/<=(.*?)>/", $content, $matches);
         foreach($matches[1] as $match)
         {
             $key = str_replace(" ", "", $match);
-            $content = preg_replace("(<=".$match."=>)", $data[$key], $content, 1);
+            if(isset($data[$key]) == false && $fatal == true)
+            {
+                throw new Exception($key." not specified in data.");
+            }
+            else if(isset($data[$key]) == false && $fatal == false)
+                continue;
+            $content = preg_replace("/<=".$match.">/", $data[$key], $content, 1);
         }
 
-        echo $content;
         return $content;
     }
 }
--- Src/test.php
+++ Src/test.php
@@ -9,5 +9,15 @@
 include_once ("Core/Template.php");
 
 $data = array();
-$data["Caca"] = "salut";
-Template::prepare("test.html", $data);
+$data["name"] = "George";
+$data["name1"] = "Vincent";
+$data["name2"] = "Cloclo";
+
+$data["people"] = array();
+$data["people"][0]["name"] = "Celia";
+$data["people"][0]["surname"] = "Paque";
+$data["people"][1]["name"] = "Clovis";
+$data["people"][1]["surname"] = "Portron";
+
+$content = Template::open("test.html");
+echo Template::prepare($content, $data);
